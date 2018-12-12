09feb756ccd6df351e18e655d994ca57fe3130e4
Daniel Johansson <johansson.gd@gmail.com>
2018-12-12 22:08:55 +0100

    Två lager innan Tuple

---
+++ NamnApp/Data/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UserRepository
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public UserRepository(string firstname, string lastname)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+        }
+
+        public 
+    }
+}
--- NamnApp/NamnApp/Program.cs
+++ NamnApp/NamnApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
 
 namespace NamnApp
 {
@@ -10,6 +11,12 @@ namespace NamnApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Firstname: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Lastname: ");
+            string last = Console.ReadLine();
+
+            UserRepository user = new UserRepository(first, last);
         }
     }
 }
